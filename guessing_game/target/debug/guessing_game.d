8470b9dca81281956a49bf5b4c60da7517717830
Amyat103 <zhoudavid7312@gmail.com>
2025-03-01 01:55:36 -0500

    started number_guess in rust

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/david/Desktop/learn_rust/guessing_game/target/debug/guessing_game: /Users/david/Desktop/learn_rust/guessing_game/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/david/Desktop/learn_rust/hello_cargo/target/debug/hello_cargo: /Users/david/Desktop/learn_rust/hello_cargo/src/main.rs
